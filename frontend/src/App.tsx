c85e81b97be589c23ac0b25e1c35e6baf056e641
shkonmrdd <54889360+shkonmrdd@users.noreply.github.com>
2025-03-05 23:36:59 +0300

    Drag and drop v0

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,30 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
 import { Excalidraw } from "@excalidraw/excalidraw";
 
 function App() {
+  const handleDragOver = (event: DragEvent) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event: DragEvent) => {
+    event.preventDefault();
+    // @ts-expect-error The event.dataTransfer.files property is actually there, but TypeScript doesn't know it.
+    const files = event.dataTransfer.files;
+    if (files.length > 0) {
+      console.log(`Dropped ${files.length} file(s):`, Array.from(files));
+    }
+  };
+
   return (
     <>
-      <div style={{ 
+      <div
+        style={{
           width: "100vw",
           height: "100vh",
           display: "flex",
-          }}>
+        }}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
         <Excalidraw />
       </div>
     </>
