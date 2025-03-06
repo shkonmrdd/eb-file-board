abae9f7284616c02d1ab5d0b31e3642abc27879d
shkonmrdd <54889360+shkonmrdd@users.noreply.github.com>
2025-03-07 01:27:00 +0300

    Clean up

--- frontend/src/pages/MarkdownViewerPage.tsx
+++ frontend/src/pages/MarkdownViewerPage.tsx
@@ -1,47 +1,46 @@
-// frontend/src/pages/MarkdownViewerPage.tsx
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import MDEditor from '@uiw/react-md-editor';
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import MDEditor from "@uiw/react-md-editor";
 
 const MarkdownViewerPage: React.FC = () => {
   const params = useParams();
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>("");
 
-  // Fetch markdown content when path parameters change
   useEffect(() => {
-    // const filename = params.url
-    // const filePath = `/files/${encodeURIComponent(filename)}`;
+    const fetchData = async () => {
+      try {
+        const urlParams = new URLSearchParams(location.search);
+        const url = urlParams.get("url") ?? "";
 
-    const urlParams = new URLSearchParams(location.search);
-    const url = urlParams.get("url") ?? "";
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("File not found");
+        }
 
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) throw new Error('File not found');
-        return response.text();
-      })
-      .then(text => setValue(text))
-      .catch(() => setValue('# File Not Found'));
+        const text = await response.text();
+        setValue(text);
+      } catch (error) {
+        setValue("# File Not Found");
+        console.error(error);
+      }
+    };
+
+    fetchData();
   }, [params]);
 
-  useEffect(() => {
-    console.log("URL params:", params);
-    console.log("Markdown content:", value);
-  }, [value]);
-
   return (
-    <div 
-    style={{
-      width: "100vw",
-      height: "100vh",
-    }}>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
       <MDEditor
         value={value}
-        onChange={(value) => setValue(value || '')}
+        onChange={(value) => setValue(value || "")}
         preview="edit"
         style={{
-          minHeight: 'calc(100vh - 1px)', // Yeah, I know
+          minHeight: "calc(100vh - 1px)", // Yeah, I know
         }}
       />
       {/* <MDEditor.Markdown source={value} style={{ whiteSpace: 'pre-wrap' }} /> */}
