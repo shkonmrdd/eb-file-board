10157ddc2e1f89a43cf74e9e574ee8acd24727b6
shkonmrdd <54889360+shkonmrdd@users.noreply.github.com>
2025-03-19 09:40:02 +0300

    Fix the backend URL

--- frontend/src/pages/FileBoardPage.tsx
+++ frontend/src/pages/FileBoardPage.tsx
@@ -8,23 +8,29 @@ import { useDragAndDrop } from "../hooks/useDragAndDrop";
 import { debounce } from "lodash";
 import { socket } from "../socket";
 import { ExcalidrawElement } from "@excalidraw/excalidraw/element/types";
-import { useParams } from 'react-router';
+import { useParams } from "react-router";
 
 const Board = () => {
   const params = useParams();
   const boardName = params.boardName ?? "main";
 
-  const [excalidrawAPI, setExcalidrawAPI] = 
+  const [excalidrawAPI, setExcalidrawAPI] =
     useState<ExcalidrawImperativeAPI | null>(null);
-  const { handleDrop, cursorPositionRef } = useDragAndDrop({ excalidrawAPI, boardName: boardName });
-  const [initialState, setInitialState] = 
+  const { handleDrop, cursorPositionRef } = useDragAndDrop({
+    excalidrawAPI,
+    boardName,
+  });
+  const [initialState, setInitialState] =
     useState<ExcalidrawInitialDataState | null>(null);
 
   useEffect(() => {
     const loadInitialState = async () => {
       try {
-        const boardUrl = `http://localhost:3001/files/${boardName}/board.json`;
-        const response = await fetch(boardUrl);
+        const boardRelative = `/files/${boardName}/board.json`;
+        const boardFull = "http://localhost:3001" + boardRelative;
+
+        const url = import.meta.env.PROD ? boardRelative : boardFull;
+        const response = await fetch(url);
 
         if (!response.ok) {
           setInitialState({});
@@ -36,6 +42,7 @@ const Board = () => {
         console.log("Loaded board state:", state);
       } catch (error) {
         console.error("Failed to load board state:", error);
+        setInitialState({});
       }
     };
     loadInitialState();
@@ -43,11 +50,13 @@ const Board = () => {
 
   const debouncedUpdateState = useCallback(
     debounce((elements, appState) => {
-      const elementsNew = elements.filter((element: ExcalidrawElement) => element.isDeleted !== true);
+      const elementsNew = elements.filter(
+        (element: ExcalidrawElement) => element.isDeleted !== true
+      );
       const payload = {
         board: { elements: elementsNew, appState },
         boardName,
-      }
+      };
       socket.emit("update-state", payload);
       console.log("Auto-saving board state...");
     }, 250),
@@ -56,50 +65,51 @@ const Board = () => {
 
   return (
     <>
-        <div
-          id="app"
-          style={{
-            width: "100vw",
-            height: "100vh",
-            display: "flex",
-            position: "relative",
-          }}
-          
-        >
-          {initialState && (
-            <Excalidraw
-              // @ts-expect-error onDrop is not recognized by Excalidraw
-              onDrop={handleDrop}
-              excalidrawAPI={(api) => setExcalidrawAPI(api)}
-              onPointerUpdate={(pointerData) => {
-                // Update cursor position with the pointer data
-                if (pointerData.pointer) {
-                  cursorPositionRef.current = pointerData.pointer;
-                }
-              }}
-              initialData={{
-                ...initialState,
-                appState: {
-                  scrollX: initialState.appState?.scrollX,
-                  scrollY: initialState.appState?.scrollY,
-                  scrolledOutside: initialState.appState?.scrolledOutside,
-                  name: initialState.appState?.name,
-                  zoom: initialState.appState?.zoom,
-                  viewBackgroundColor: initialState.appState?.viewBackgroundColor,
-                  currentItemFontFamily: 2,
-                  currentItemRoughness: 0,
-                  zenModeEnabled: false,
-                  theme: initialState.appState?.theme ?? window.matchMedia("(prefers-color-scheme: dark)").matches
+      <div
+        id="app"
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          position: "relative",
+        }}
+      >
+        {initialState && (
+          <Excalidraw
+            // @ts-expect-error onDrop is not recognized by Excalidraw
+            onDrop={handleDrop}
+            excalidrawAPI={(api) => setExcalidrawAPI(api)}
+            onPointerUpdate={(pointerData) => {
+              // Update cursor position with the pointer data
+              if (pointerData.pointer) {
+                cursorPositionRef.current = pointerData.pointer;
+              }
+            }}
+            initialData={{
+              ...initialState,
+              appState: {
+                scrollX: initialState.appState?.scrollX,
+                scrollY: initialState.appState?.scrollY,
+                scrolledOutside: initialState.appState?.scrolledOutside,
+                name: initialState.appState?.name,
+                zoom: initialState.appState?.zoom,
+                viewBackgroundColor: initialState.appState?.viewBackgroundColor,
+                currentItemFontFamily: 2,
+                currentItemRoughness: 0,
+                zenModeEnabled: false,
+                theme:
+                  initialState.appState?.theme ??
+                  window.matchMedia("(prefers-color-scheme: dark)").matches
                     ? "dark"
                     : "light",
-                },
-              }}
-              validateEmbeddable={() => true}
-              onChange={(elements, appState) => {
-                debouncedUpdateState(elements, appState);
-              }}
-            />
-          )}
+              },
+            }}
+            validateEmbeddable={() => true}
+            onChange={(elements, appState) => {
+              debouncedUpdateState(elements, appState);
+            }}
+          />
+        )}
       </div>
     </>
   );
