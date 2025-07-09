b640ea5d475d7cdbacd7baac56122f8eacfc3fe6
shkonmrdd <54889360+shkonmrdd@users.noreply.github.com>
2025-07-09 07:04:14 +0300

    Full delete cycle

--- frontend/src/components/ConfirmationModal.tsx
+++ frontend/src/components/ConfirmationModal.tsx
@@ -29,7 +29,7 @@ const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
       <div 
-        className="absolute inset-0 bg-black bg-opacity-50 dark:bg-opacity-50"
+        className="absolute inset-0 bg-black/50 dark:bg-black/50"
         onClick={onCancel}
       />
 
